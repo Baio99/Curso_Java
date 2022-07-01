89e1d9a73af3840704686033f8f1d4d2a8028d19
BAIO117 <bryanadrian38@hotmail.com>
2022-06-30 22:09:43 -0500

    Ejercicios Basicos

---
+++ Curso de Java/Curso de Java 1er modulo/PrimerosPasos/src/primerospasos/Declaraciones_Operadores.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package primerospasos;
+
+/**
+ *
+ * @author Adrian
+ */
+public class Declaraciones_Operadores {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a = 5;
+        int b;
+        b = 7;
+        
+        //decimales
+        
+        double e=5;
+        double f=7;
+              
+
+        int c = b + a;
+        c += 6;// incremento a C +6
+
+        System.out.println(c);
+
+        c -= 5;
+
+        System.out.println(c);
+
+        double g=f/e;
+
+        System.out.println(g);
+    }
+
+}
