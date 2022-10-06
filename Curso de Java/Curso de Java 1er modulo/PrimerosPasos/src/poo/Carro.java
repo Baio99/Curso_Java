a3421f0dd946457b6ad0c4c0c8f26af1b2c1d5e3
BAIO117 <bryanadrian38@hotmail.com>
2022-10-06 17:24:29 -0500

    actualizacion POO, getters y setters

--- Curso de Java/Curso de Java 1er modulo/PrimerosPasos/src/poo/Carro.java
+++ Curso de Java/Curso de Java 1er modulo/PrimerosPasos/src/poo/Carro.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package poo;
+import java.lang.Object;
 
 /**
  *
@@ -11,11 +12,15 @@ package poo;
  */
 public class Carro {
     
-    int ruedas;
-    int largo;
-    int ancho;
-    int motor;
-    int peso;
+    private int ruedas;
+    private int largo;
+    private int ancho;
+    private int motor;
+    private int peso_plataforma;
+    private String color;
+    private int peso_total;
+    private boolean asientos_cuero, climatizador;
+  
     
     // metodo constructor
     //el metodo constructor siempre va con el nombre de la clase y con parentesis
@@ -25,17 +30,125 @@ public class Carro {
     public Carro(){
         
         ruedas=4;
-        largo= 1000;
+        largo=2000;
         ancho=300;
-        motor= 2000;
-        peso=1400;
-         
+        motor= 1600;
+        peso_plataforma=500;
         
-        
-        
+    }
+    //metodos, metodo getter, me proporcionar el valor de la propiedad osea me muestran
+    /*syntaxys, public, tipo de dato a devolver , nombre del metodo() (cualquier nombre)*{codigo + return}*/
+    
+    public String dime_datos_generalez(){
+        return   "la plaraforma del vehiculo tiene "+ ruedas + " ruedas " + " y mide "+ largo + 
+                " largo " + ancho + " ancho "+ " y un peso de plataforma de " + peso_plataforma;
         
         
     }
+    /*setter, lo que va entre parentesis son los parametros ahi va el tipo de dato que voy a usar
+    seguido del nombre de una variable */
+    public void establecer_color(String color_coche){
+      // variable declarada para el color = a la variable que hiso en el parametro  
+        color=color_coche;
+        
+    }
+    //getter
+    public String dime_color(){
+    return "el color del coche es  " + color;
+    } 
+    
+    /*setter, siempre va con void porque no devuelve nada, nonbre del metodo, (tipo de dato del parametro
+    , nombre de la variable del parametro)*/
+    public void configurar_asientos(String asientos_cuero){
+        /*this. hace referencia a la variable de la clase, no a la del parametro del setter
+        si los asientos de cuero es igual a la palabra si*/
+        this.asientos_cuero = asientos_cuero.equalsIgnoreCase("si"); /* this. asientos hace referencia a la variable de la clase, como es de tipo boolean, acepta
+        solo true y false */ // this.asientos_cuero= true
+        // si no, this.asientos_cuero= false
+        
+    }
+    /*getter, tipo de dato a devolver, nombre del metodo, y al final necesita un return*/
+    public String dime_asientos(){
+    /*this.asientos_cuero hace referencia a la variable de la clase
+        si, this.asientos_cuero es igual a true*/   
+        if (this.asientos_cuero==true) {
+      // me retorna el mensaje: el carro tiene asientos de cuero;      
+           return "El carro tiene asientos de cuero ";
+         // si no, me retorna el carro no tiene asientos de cuero   
+        }else{
+            return "El carro no tienen asientos de cuero, son estandar";
+        }
+    }
+    /*setter: void, nombre del metodo, parametros*/
+    public void configurar_climatizador(String climatizador){
+        
+        if (climatizador.equalsIgnoreCase("si")) {
+            this.climatizador=true;
+            
+        }else{
+            this.climatizador=false;
+        }
+        
+    }
+    //getter
+    public String dime_climatizador(){
+    
+            if (this.climatizador==true) {
+                
+                return "Tiene climatizador";
+                
+            }else{
+                return "no tiene climatizador";
+            }
+}
+    //setter y getter a la misma vez del peso
+    public String establecer_peso(){
+        
+        int peso_carroceria=500;
+        peso_total=peso_plataforma+peso_carroceria;
+        
+        
+        if (asientos_cuero==true) {
+            
+            peso_total=peso_total+50;  
+                     
+        }  
+        if (climatizador==true) {
+            
+            peso_total=peso_total+20;
+
+        }
+        return "El peso es: " +peso_total;
+        
+        
+    }
+    
+    /*setter y getter a la misma vez del precio*/
+    
+    public int establecer_precio(){
+        
+       int precio_final =10000;
+       
+        if (asientos_cuero==true) {
+            
+            precio_final=precio_final+2000; 
+            
+        }
+        if (climatizador==true) {
+            
+            precio_final=precio_final+1500;
+            
+        }
+        return precio_final;
+        
+        
+    }
+    
+    
+    
+    
+    
+    
     
     
 }
--- Curso de Java/Curso de Java 1er modulo/PrimerosPasos/src/poo/Uso_Carro.java
+++ Curso de Java/Curso de Java 1er modulo/PrimerosPasos/src/poo/Uso_Carro.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package poo;
+import javax.swing.*;
 
 /**
  *
@@ -17,7 +18,7 @@ public class Uso_Carro {
     public static void main(String[] args) {
         
         /*nombre de la clase creada donde esta el constructor (osea carro)
-        , nombre de una variable cualquiera, =, new, nombre de la clase creada donde esta el constructor (osea carro) */
+        , nombre del obejto ( el que o desse), =, new, nombre de la clase creada donde esta el constructor (osea carro) */
         
         Carro renault= new Carro(); //instanciar una clase, ejemplar de clase
         
@@ -30,13 +31,36 @@ public class Uso_Carro {
         renault.ruedas=4;*/
         
         // o imprimir los datos guardados de la clase carro
-        System.out.println(" este coche tiene " + renault.ancho + " ancho ");
+        /*System.out.println(" este coche tiene " + renault.ancho + " ancho ");
         System.out.println(renault.largo + " largo");
         System.out.println(renault.motor + " su motor");
         System.out.println(renault.peso + " de peso ");
         System.out.println(renault.ruedas + " ruedas");
         
         
-    }
+    }*/
+        //aplicando metodos, getter
+        renault.establecer_color(JOptionPane.showInputDialog("Introduce el color "));// me establece el color del metodo, si no pongo estee me saldria null
+        System.out.println(renault.dime_datos_generalez());
+        System.out.println(renault.dime_color());
+        /*primero llamo al setter para asignar el valor
+        entonces. nombre del objeto.nombre del metodo
+        renault.configurar_asientos=si o no, igualmente uso JOPTION PANE, para que mi respuesta
+        reciba un cuadro de dialogo grafico ( es opcional)*/
+        renault.configurar_asientos(JOptionPane.showInputDialog("¿Tiene asientos de cuero?"));
+        // me imprime el getter, con la condicion que tiene
+        System.out.println(renault.dime_asientos());
+          /*primero llamo al setter para asignar el valor
+        entonces. nombre del objeto.nombre del metodo
+        renault.configurar_climatizador=si o no, igualmente uso JOPTION PANE, para que mi respuesta
+        reciba un cuadro de dialogo grafico ( es opcional) */
+        renault.configurar_climatizador(JOptionPane.showInputDialog("¿Tiene Climatizador?"));
+        //lamo al setter
+        System.out.println(renault.dime_climatizador());
+        
+        //aqui uso los que son getter y setter a la misma vez, por eso no hace falta primero llamar al setter
+        System.out.println(renault.establecer_peso());
+        System.out.println("El precio total es " + renault.establecer_precio());
     
+}
 }
